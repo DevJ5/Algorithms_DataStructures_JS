255c01c516a2b21d18058aa8149a789980ad9a08
DevJ5 <idevelopjs@gmail.com>
2019-09-24 08:50:59 +0200

    Max Binary Heap

--- DataStructures/BinaryHeap.js
+++ DataStructures/BinaryHeap.js
@@ -6,16 +6,23 @@ class MaxBinaryHeap {
     insert(val) {
         this.values.push(val);
         let index = this.values.length - 1;
-        let nodeValue = val;
 
-        while (nodeValue > this.values[getParentIndex(index)]) {
-            swap(index, getParentIndex(index));
-            nodeValue = this.values[getParentIndex(nodeValue)];
+        let swapped = true;
+        let parentIndex;
+
+        while (swapped) {
+            swapped = false;
+            parentIndex = this.getParentIndex(index);
+            if (val > this.values[parentIndex]) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+                swapped = true;
+            }
         }
     }
 
     getParentIndex(index) {
-        const parent = (index - 1) / 2;
+        const parent = Math.floor((index - 1) / 2);
         return parent;
     }
 
@@ -25,3 +32,17 @@ class MaxBinaryHeap {
         this.values[parentIndex] = temp;
     }
 }
+
+const mbh = new MaxBinaryHeap();
+mbh.insert(41);
+mbh.insert(39);
+mbh.insert(33);
+mbh.insert(18);
+mbh.insert(27);
+mbh.insert(12);
+mbh.insert(55);
+mbh.insert(1);
+mbh.insert(45);
+mbh.insert(199);
+
+console.log(mbh.values);
