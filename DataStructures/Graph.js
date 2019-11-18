ea62218de9fbabef95b4d905e04c55312b4736c5
DevJ5 <idevelopjs@gmail.com>
2019-11-18 07:37:36 +0100

    Graphs

---
+++ DataStructures/Graph.js
@@ -0,0 +1,37 @@
+// Adjacency Matrix
+/*
+       A | B | C
+   A | 0 | 0 | 1
+   B | 0 | 0 | 1
+   C | 1 | 1 | 0
+*/
+
+// Adjacency List
+/*
+    [
+        [2],  A
+        [2],  B
+        [0,1] C
+    ]
+*/
+
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+        }
+    }
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1);
+        }
+    }
+}
